4e56f1b02632e6f1e98bba2769627526a7081b35
Meierschlumpf <meierschlumpf@gmail.com>
2022-12-28 23:23:00 +0100

    fix: translations not working on vercel #12

--- apps/nextjs/next-i18next.config.js
+++ apps/nextjs/next-i18next.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
   i18n: {
@@ -6,6 +8,7 @@ module.exports = {
     fallbackLng: "en",
     localeDetection: true,
     returnEmptyString: false,
+    localePath: path.resolve("./public/locales"),
   },
   reloadOnPrerender: process.env.NODE_ENV === "development",
   react: { useSuspense: false },
